43483a43fb85dc94501b0e4f28528da70640e21c
Elvis-codeur <fabriceagbalenyo@gmail.com>
2025-04-12 22:53:57 +0200

    #25 - Scrap channel data from last date to current date

--- src/Instrumentum_sanae_doctrinae/cli_interface/telegram/telegram_command_executer.py
+++ src/Instrumentum_sanae_doctrinae/cli_interface/telegram/telegram_command_executer.py
@@ -45,3 +45,41 @@ def scrap_channel_text_message_from_date_to_date(context:click.Context,
 
     
  
+ 
+ 
+
+
+@click.command()
+@click.argument("api_id",type=str,required = True)
+@click.argument("api_hash",type = str,required = True)
+@click.argument("phone_number",type=str,required = True)
+@click.argument("group_username",type = str, required = True)
+@click.argument("output_folder",type=str,required = True)
+@click.pass_context
+def scrap_channel_text_message_from_date_to_date(context:click.Context,
+                               api_id,api_hash,
+                               phone_number,
+                               date_begin,date_end,
+                               group_username,
+                               output_folder:str,
+                               overwrite):
+    
+    
+    telegram_client = MyTelegramClient(api_id,api_hash,phone_number)
+    
+    scraper =  ScrapTelegramGroup(group_username,telegram_client,output_folder)
+    
+    # Load the content of the file existing now 
+    #scraper.load_file_content()
+    
+    #print(scraper.date_last_scraping)
+    
+    telegram_client.client.loop.run_until_complete(scraper.scrap_from_last_scraping_date(
+        date_begin=date_begin,
+        date_end=date_end
+    ))
+    
+    scraper.update_file_content()
+
+    
+ 
